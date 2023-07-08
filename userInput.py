70d051e741ef04c00fd791c7eebccf4d0f914d2f
callumconnor-dev <132172088+callumconnor-dev@users.noreply.github.com>
2023-07-09 00:03:35 +0100

    Update userInput.py

--- userInput.py
+++ userInput.py
@@ -1,1 +1,5 @@
-print("Your name is " + str(len(input("What is your name?\n"))) + " characters long.")
+print("Your name is " + str(len(input("What is your name?\n"))) + " characters long.") # One Line Solution
+
+name = input("What is your surname?\n") # Multi Line Solution
+length = len(name)
+print("Your surname is " + str(length) + " characters long.")
